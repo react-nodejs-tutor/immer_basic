14c0b365a8d53d0833ba28986351c8b264b9f647
koojaa <93jwkoo@gmail.com>
2020-04-04 13:05:37 +0900

    immer tutorials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	id = 2;
+	state = {
+		input: '',
+		todos: [
+			{
+				id: 1,
+				text: '제목1',
+			},
+			{
+				id: 2,
+				text: '제목2',
+			},
+		],
+	};
+	handleChange = () => {};
+	handleInsert = () => {};
+	handleToggle = () => {};
+	handleRemove = () => {};
+	render() {
+		return (
+			<div>
+				<div>
+					<input />
+					<button>추가</button>
+				</div>
+				<ul>
+					{this.state.todos.map((todo) => (
+						<li
+							key={todo.id}
+							style={{
+								textDecoration: todo.done ? 'line-through' : 'none',
+							}}
+						>
+							{todo.text}
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
-
 export default App;
